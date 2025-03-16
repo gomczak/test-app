224863a0bd35f0d681510d7867008d5f8dc24b6c
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2025-03-16 11:16:51 +0100

    added protecting routes

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/login');
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = { requireAuth };
